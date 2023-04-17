926d30f9fb85a38cc756ebf3c8804033c30b7a3e
AndrewKehl <60129754+AndrewKehl@users.noreply.github.com>
2023-04-16 23:10:52 -0700

    all changes

--- 2_UI/src/environments/environment.ts
+++ 2_UI/src/environments/environment.ts
@@ -8,15 +8,15 @@ export const environment = {
 
   // TODO: This id can be retrieved in output section of the cognito ui
   // cloud formation stack.
-  cognitoIdentityPoolId: '',
+  cognitoIdentityPoolId: 'eu-west-1:2c53e814-2c25-4ce9-9c39-a96769c462da',
 
   // TODO: Facebook app id can be retrieved from the application in your
   // facebook developer account.
-  facebookAppId: '',
+  facebookAppId: '929902708321511',
 
   // TODO: The API URL is available in the API Gateway console under Stage
   // NOTE: don't forget trailing "/"  For example:
   // https://api.example.com/prod/
-  ticketAPI: ''
+  ticketAPI: 'https://api.hockeyanalytics.net/'
 
 };
